80ec7aa6d415499b75802a06d5b42894aebe0e2c
Bjarneboi <schroeder.bjarne97@gmail.com>
2023-05-27 12:31:11 +0200

    Adding datalass DatabaseSettings.

---
+++ tests/test_db_settings.py
@@ -0,0 +1,56 @@
+import pytest
+
+from wg_gesucht.wg_gesucht.db_settings import DatabaseSettings
+
+
+class TestDatabaseSettings:
+    def test_db_name_wrong_type(self):
+        with pytest.raises(TypeError):
+            DatabaseSettings(
+                db_name=1,
+                connection_uri="mongodb://localhost:27017",
+            )
+
+    def test_db_name_empty(self):
+        with pytest.raises(ValueError):
+            DatabaseSettings(
+                db_name="",
+                connection_uri="mongodb://localhost:27017",
+            )
+
+    def test_db_name_none(self):
+        with pytest.raises(ValueError):
+            DatabaseSettings(
+                db_name=None,
+                connection_uri="mongodb://localhost:27017",
+            )
+
+    def test_connection_uri_wrong_type(self):
+        with pytest.raises(TypeError):
+            DatabaseSettings(
+                db_name="wg_gesucht",
+                connection_uri=1,
+            )
+
+    def test_connection_uri_empty(self):
+        with pytest.raises(ValueError):
+            DatabaseSettings(
+                db_name="wg_gesucht",
+                connection_uri="",
+            )
+
+    def test_connection_uri_none(self):
+        with pytest.raises(ValueError):
+            DatabaseSettings(
+                db_name="wg_gesucht",
+                connection_uri=None,
+            )
+
+    def test_db_settings(self):
+        settings: DatabaseSettings = DatabaseSettings(
+            db_name="wg_gesucht",
+            connection_uri="mongodb://localhost:27017",
+        )
+
+        assert settings.db_name == "wg_gesucht"
+        assert settings.connection_uri == "mongodb://localhost:27017"
---
+++ wg_gesucht/wg_gesucht/db_settings.py
@@ -0,0 +1,54 @@
+import logging
+from dataclasses import dataclass
+
+logger = logging.getLogger()
+
+
+@dataclass
+class DatabaseSettings:
+    db_name: str
+    connection_uri: str
+
+    def __init__(
+        self,
+        db_name: str,
+        connection_uri: str,
+    ):
+        self._validate_init_params(
+            db_name=db_name,
+            connection_uri=connection_uri,
+        )
+        self.db_name = db_name
+        self.connection_uri = connection_uri
+
+    def _validate_init_params(
+        self,
+        db_name: str,
+        connection_uri: str,
+    ):
+        if not db_name:
+            raise ValueError("db_name must be set")
+
+        if not isinstance(db_name, str):
+            raise TypeError("db_name must be of type str")
+
+        if not connection_uri:
+            raise ValueError("connection_uri must be set")
+
+        if not isinstance(connection_uri, str):
+            raise TypeError("connection_uri must be of type str")
+
+
+def load_db_settings(
+    db_name: str,
+    connection_uri: str,
+) -> DatabaseSettings:
+    try:
+        return DatabaseSettings(
+            db_name=db_name,
+            connection_uri=connection_uri,
+        )
+    except (ValueError, TypeError) as e:
+        logger.error("Failed to load database settings. Please check input.")
+        logger.error(f"Error: {e}")
+        raise e
