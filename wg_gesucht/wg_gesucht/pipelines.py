2bf3b9f99b9eba6a415c75b208836fe6099885ff
Bjarneboi <schroeder.bjarne97@gmail.com>
2023-06-10 09:05:27 +0200

    Adding documentation for the MongoDB pipeline.

--- wg_gesucht/wg_gesucht/pipelines.py
+++ wg_gesucht/wg_gesucht/pipelines.py
@@ -12,6 +12,7 @@ from pymongo.database import Database
 from pymongo.errors import ServerSelectionTimeoutError, WriteError
 from wg_gesucht.db_settings import DatabaseSettings
 from wg_gesucht.items import FlatItem
+from wg_gesucht.spiders.wg_gesucht import WgGesuchtSpider
 
 
 class WgGesuchtPipeline:
@@ -21,6 +22,7 @@ class WgGesuchtPipeline:
 
     @classmethod
     def from_crawler(cls, crawler):
+        """Creates a new instance of the pipeline with the given crawler settings."""
         db_settings: Optional[DatabaseSettings] = crawler.settings.get("DB_SETTINGS")
         if not db_settings:
             raise ValueError("DB_SETTINGS must be set in settings.py")
@@ -32,6 +34,14 @@ class WgGesuchtPipeline:
     def __init__(
         self, connection_uri: str, db_name: str, collection_name: str = "flats"
     ):
+        """
+        Initializes the pipeline with the given connection parameters.
+
+        Args:
+            connection_uri: The connection URI to the MongoDB database.
+            db_name: The name of the database to use.
+            collection_name: The name of the collection to use.
+        """
         self._client = MongoClient(connection_uri, timeoutMS=10000)
         self._check_database_connection(client=self._client)
 
@@ -40,6 +50,7 @@ class WgGesuchtPipeline:
         self._collection.create_index("id", name="flat_id", unique=True)
 
     def _check_database_connection(self, client: MongoClient):
+        """Pings the database and raises an error if the connection fails."""
         try:
             logging.info("Pinging database to check connection.")
             client.admin.command("ping")
@@ -49,7 +60,10 @@ class WgGesuchtPipeline:
                 "Please check given connection parameters."
             )
 
-    def process_item(self, item: FlatItem, spider):
+    def process_item(self, item: FlatItem, spider: WgGesuchtSpider) -> FlatItem:
+        """Processes the given item and persists it to the database.
+        Duplicate items are ignored. Other errors are logged.
+        """
         try:
             self._collection.insert_one(ItemAdapter(item).asdict())
         except WriteError as e:
